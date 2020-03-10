78d009016d6ec4345309799fd8aa6954d413e508
Elizabeth Brown <ej2brown@edu.uwaterloo.ca>
2020-03-10 02:57:42 +0000

    spinner2 where setTimeOut is in a for of loop

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+const spinner = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+const delay = 200;
+let delaySum = 100;
+
+for (const line of spinner) {
+  setTimeout(() => {
+    process.stdout.write(line);
+  }, delaySum)
+  delaySum += delay;
+} 
